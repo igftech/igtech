c7e19aa0fca9c2555553e1db48d5588d57db7796
igftech <theigiehonmathstournament@gmail.com>
2025-09-17 05:35:02 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from '@sanity/cli';
+import schemaTypes from './src/schemas'   // if sanity.config.ts is in the root
+
 
 export default defineConfig({
   projectId: 'your-project-id', // Replace with your Sanity project ID
   dataset: 'production',
   plugins: ['@sanity/vision'],
   schema: {
-    types: [] // Schema types will be imported from src/schemas
+    types: [
+      ...schemaTypes
+           ] // Schema types will be imported from src/schemas
   },
   studio: {
     basePath: '/studio'
