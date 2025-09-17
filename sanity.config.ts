bde719c5958ac99953d9d45a9c03d7c2f4350c67
igftech <theigiehonmathstournament@gmail.com>
2025-09-17 07:24:50 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from '@sanity/cli';
-import { schemaTypes } from './src/schemas';
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from '../src/schemas'
 
 export default defineConfig({
-  projectId: 'am22ijkm', // Replace with your Sanity project ID
+  name: 'igiehon-foundation',
+  title: 'Igiehon Foundation CMS',
+
+  projectId: 'am22ijkm', // Replace with your actual Sanity project ID
   dataset: 'production',
-  plugins: ['@sanity/vision'],
+
+  plugins: [structureTool(), visionTool()],
+
   schema: {
-    types: schemaTypes
+    types: schemaTypes,
   },
-  studio: {
-    basePath: '/studio'
-  }
-});
+})
